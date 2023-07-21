04af1d934a7d8bc45598a68b08e164351f4b11b3
Дмитрий Чепиницкий <chp-dn@yandex.ru>
2023-07-21 17:24:53 +0300

    Last redaction

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,31 +1,55 @@
-const deadCount = document.getElementById("dead");
-const lostCount = document.getElementById("lost");
+// const deadCount = document.getElementById("dead");
+// const lostCount = document.getElementById("lost");
 
-function reloadGame() {
-  deadCount.textContent = 0;
-  lostCount.textContent = 0;
-}
+// function reloadGame() {
+//   deadCount.textContent = 0;
+//   lostCount.textContent = 0;
+// }
 
-getHole = (index) => document.getElementById(`hole${index}`);
+// getHole = (index) => document.getElementById(`hole${index}`);
 
-for (let i = 1; i <= 9; i++) {
-  let hole = getHole(i);
+// for (let i = 1; i <= 9; i++) {
+//   let hole = getHole(i);
 
-  hole.onclick = () => {
-    if (hole.classList.contains("hole_has-mole")) {
-      deadCount.textContent++;
+//   hole.onclick = () => {
+//     if (hole.classList.contains("hole_has-mole")) {
+//       deadCount.textContent++;
+//     } else {
+//       lostCount.textContent++;
+//     }
+
+//     if (deadCount.textContent >= 10) {
+//       alert("Победа!");
+//       reloadGame();
+//     }
+
+//     if (lostCount.textContent >= 5) {
+//       alert("Поражение!");
+//       reloadGame();
+//     }
+//   };
+// }
+// второй вариант от преподавателя (не работает)
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+const holes = document.querySelectorAll(".hole");
+
+for (let hole of holes) {
+  hole.onclick = function () {
+    if (hole.classList.contains(".hole")) {
+      dead.innerText++;
     } else {
-      lostCount.textContent++;
+      lost.innerText++;
     }
 
-    if (deadCount.textContent >= 10) {
-      alert("Победа!");
-      reloadGame();
-    }
-
-    if (lostCount.textContent >= 5) {
-      alert("Поражение!");
-      reloadGame();
+    if (dead.innerText === "10") {
+      alert("Победа");
+      dead.innerText = 0;
+      lost.innerText = 0;
+    } else if (lost.innerText === "5") {
+      alert("Вы проиграли");
+      dead.innerText = 0;
+      lost.innerText = 0;
     }
   };
 }
