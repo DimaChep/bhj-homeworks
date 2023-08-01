87a3e3d91e430864aedcfc4979272b82d93b754b
Дмитрий Чепиницкий <chp-dn@yandex.ru>
2023-08-01 09:55:28 +0300

    Задачи готовы к проверке

---
+++ document-structure/todo/task.js
@@ -0,0 +1,23 @@
+const taskInput = document.getElementById("task__input");
+const tasksAdd = document.getElementById("tasks__add");
+const tasksList = document.getElementById("tasks__list");
+
+tasksAdd.addEventListener("click", (e) => {
+  if (!taskInput.value) {
+    return;
+  } else {
+    const task = document.createElement("div");
+    task.classList.add("task");
+    task.innerHTML = `<div class="task__title">${taskInput.value}</div><a href="#" class="task__remove">&times;</a>`;
+    tasksList.appendChild(task);
+    taskInput.value = "";
+    e.preventDefault();
+
+    const taskRemove = document.querySelectorAll(".task__remove");
+    taskRemove.forEach((i) => {
+      i.addEventListener("click", () => {
+        i.closest(".task").remove();
+      });
+    });
+  }
+});
